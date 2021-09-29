fd980a1190c8c867d0ee55137af1495d3a201c5d
Juan <wardkjuan44@gmail.com>
2021-09-29 10:06:04 -0500

    hola

--- src/main/java/com/example/demo/model/Cliente.java
+++ src/main/java/com/example/demo/model/Cliente.java
@@ -26,17 +26,14 @@ public class Cliente {
     @Column(name = "telefono", nullable = true, length =10)
     private String telefono;
 
-    @Column(name = "email", nullable = true, length = 55)
+    /*@Column(name = "email", nullable = true, length = 55)
     private String email;
 
     @Column(name = "username", nullable = true)
     private String username;
 
     @Column(name = "password", nullable = true, length = 55)
-    private String password;
-
-    @Transient
-    private String confirmPassword;
+    private String password;*/
 
     public Integer getIdCliente() {
         return idCliente;
@@ -76,37 +73,5 @@ public class Cliente {
 
     public void setTelefono(String telefono) {
         this.telefono = telefono;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
     }
 }
